cfdd72bc1fbc050b3fdfe9df1ad6b40e4800b2cb
Ichisada Shioko <ichisadashioko@gmail.com>
2020-01-02 17:17:29 +0700

    :rocket: working template

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { systemReducer } from './system/reducers'
 import { chatReducer } from './chat/reducers'
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
     system: systemReducer,
     chat: chatReducer,
 })
